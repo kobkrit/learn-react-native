2b7fae02745ac1a25ac6d05188562bb2eb2bbcf4
Kobkrit Viriyayudhakorn <kobkrit@gmail.com>
2016-09-26 16:03:41 +0700

    Add Navigation bar

--- l7_movie/index.js
+++ l7_movie/index.js
@@ -23,17 +23,31 @@ class l7_movie extends Component {
       <Navigator style={styles.container}
         initialRoute={routes[0]}
         initialRouteStack={routes}
-        renderScene={(route, navigator) =>
-
-        <TouchableHighlight onPress={() => {
-            if (route.index == 0) {
-              navigator.push(routes[1]);
-            } else {
-              navigator.pop();
-            }
-          }}>
-          <Text>Hello {route.title}!</Text>
-        </TouchableHighlight>}
+        renderScene={
+          (route, navigator) =>
+            <TouchableHighlight onPress={() => {
+                if (route.index == 0) {
+                  navigator.push(routes[1]);
+                } else {
+                  navigator.pop();
+                }
+              }}>
+              <Text>Hello {route.title}!</Text>
+            </TouchableHighlight>
+        }
+        navigationBar={
+         <Navigator.NavigationBar
+           routeMapper={{
+             LeftButton: (route, navigator, index, navState) =>
+              { return (<Text>Cancel</Text>); },
+             RightButton: (route, navigator, index, navState) =>
+               { return (<Text>Done</Text>); },
+             Title: (route, navigator, index, navState) =>
+               { return (<Text>Awesome Nav Bar</Text>); },
+           }}
+           style={{backgroundColor: 'gray'}}
+         />
+      }
     />
     );
   }
