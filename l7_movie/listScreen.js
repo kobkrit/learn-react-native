4c3706d7775ef1b6b6c941e0b33a91b94401c420
Kobkrit Viriyayudhakorn <kobkrit@gmail.com>
2016-09-27 09:41:07 +0700

    Complete row in ListView

--- l7_movie/listScreen.js
+++ l7_movie/listScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {TouchableHighlight, AppRegistry, ListView, StyleSheet, Text, View} from 'react-native';
+import {TouchableHighlight, AppRegistry, ListView, StyleSheet, Text, View, Image} from 'react-native';
 import api from './api';
 
 class ListScreen extends Component {
@@ -9,7 +9,7 @@ class ListScreen extends Component {
    this.state = {
      dataSource: ds.cloneWithRows([]),
    };
-   api.search('batman').then((data)=>{
+   api.search('harry').then((data)=>{
      this.setState({dataSource: ds.cloneWithRows(data)});
    });
   }
@@ -20,7 +20,18 @@ class ListScreen extends Component {
         dataSource={this.state.dataSource}
         renderRow={(rowData) => {
           return (
-            <Text>{rowData.Title}</Text>
+            <View style={styles.row}>
+                <View style={{flex:3}}>
+                  <Image style={styles.image} source={{uri: rowData.Poster}}/>
+                </View>
+                <View style={{flex:10, padding: 10}}>
+                  <Text style={styles.title}>{rowData.Title} ({rowData.Year})</Text>
+                </View>
+                <View style={{flex:1, justifyContent:'center'}}>
+                  <Text style={styles.title}>></Text>
+                </View>
+            </View>
+
           );
         }}
         renderSeparator={(sectionID, rowID, adjacentRowHighlighted) =>
@@ -35,6 +46,16 @@ const styles = StyleSheet.create({
     padding: 10,
     paddingTop:65,
     flex:1
+  },
+  row:{
+    flexDirection: 'row',
+    height: 100
+  },
+  image:{
+    height: 100
+  },
+  title:{
+    fontSize: 20
   }
 });
 
